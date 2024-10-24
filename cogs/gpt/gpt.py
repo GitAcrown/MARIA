552a64cbf97be5e49371a6f2a55cef040d1235bc
GitAcrone <acrown@outlook.fr>
2024-10-24 03:01:00 +0200

    Refactor GPT class to update tool usage tracking and add new emoji reactions for tool access

--- cogs/gpt/gpt.py
+++ cogs/gpt/gpt.py
@@ -784,9 +784,9 @@ class GPT(commands.Cog):
 
                 # Ajout d'un emoji si un outil a été utilisé (on a noté le message d'outil juste avant)
                 if completion.tool_used in ('get_user_info', 'get_all_user_info'):
-                    content += "\n-# Recherche dans les notes effectuée"
+                    content += "\n-# <:search:1298806107900477440> Recherche dans les notes effectuée"
                 elif completion.tool_used == 'set_user_info':
-                    content += "\n-# Notes mises à jour"
+                    content += "\n-# <:save:1298806120122548224> Notes mises à jour"
                 
                 await message.reply(content, mention_author=False, suppress_embeds=True, allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False, replied_user=True))
                 
